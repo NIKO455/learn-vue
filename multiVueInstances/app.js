557b3322624a27b4eda30d2fc67b99216dcf4c1d
Bhupendra Kathayat <bhupendrakathayatop@gmail.com>
2025-04-13 21:58:01 +0545

    multi vue instance

--- multiVueInstances/app.js
+++ multiVueInstances/app.js
@@ -1,10 +1,5 @@
 /*
- * Expressions: It is the code inside the curly brackets. Eg: {{ firstName }}
- * String Interpolation: It is process of replacing placeholder into string value. Eg: inplace of {{ firstName }} replacing with the actual data is interpolation
- *
- *
- * To access the value into the app we must declare into the data.
- * And the value declared inside the data can't be accessed outside the #app id wrapper
+ *  if we assing two id or class that is mounting to the app then the first one is load and the second one is removed by the vue it self
  *
  * */
 
@@ -17,3 +12,13 @@ Vue.createApp({
     }
   }
 }).mount('#app');
+
+Vue.createApp({
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+
+    }
+  }
+}).mount('#app2');
--- workingWithData/app.js
+++ workingWithData/app.js
@@ -1,5 +1,10 @@
 /*
- * 
+ * Expressions: It is the code inside the curly brackets. Eg: {{ firstName }}
+ * String Interpolation: It is process of replacing placeholder into string value. Eg: inplace of {{ firstName }} replacing with the actual data is interpolation
+ *
+ *
+ * To access the value into the app we must declare into the data.
+ * And the value declared inside the data can't be accessed outside the #app id wrapper
  *
  * */
 
